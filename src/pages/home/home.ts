7b59c8f824592763f933e229e5af7497d97bc666
Sokunthaneth Chhoy <chhoysokunthaneth@gmail.com>
2018-11-11 11:05:28 +0700

    Version 1.0.0

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private iab: InAppBrowser) {
+    this.openWebPage()
   }
-
+  
+  openWebPage(){
+    const options : InAppBrowserOptions = {
+      zoom: 'no',
+      hidden: 'yes',
+    }
+    this.iab.create('https://mts.csx.com.kh', '_blank', options)
+  }
 }
